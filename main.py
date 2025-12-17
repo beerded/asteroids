36998b6f5d6118c23c520a8441d3d7ec97f9f8f6
A P <67877471+beerded@users.noreply.github.com>
2025-12-17 18:21:34 +0200

    Set framerate
    Max framerate = 60 FPS, therefore max deltatime is 1/60th of a
    second.

--- main.py
+++ main.py
@@ -9,16 +9,19 @@ def main():
 
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
         log_state()
         screen.fill("black")
-
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
