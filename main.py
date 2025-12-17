4831083089cd37c24003377edef44aeade2dd2fb
A P <67877471+beerded@users.noreply.github.com>
2025-12-17 23:30:04 +0200

    Update code to use pygame groups
    updatable and drawable groups contain things that are updatable and
    drawable, so added them there

--- main.py
+++ main.py
@@ -15,17 +15,23 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         log_state()
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
