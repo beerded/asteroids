d877884d9b25867f474dd3260527fd1ac52cd02a
A P <67877471+beerded@users.noreply.github.com>
2025-12-17 23:29:24 +0200

    Minor prettifying

--- player.py
+++ player.py
@@ -1,5 +1,10 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import (
+        PLAYER_RADIUS,
+        LINE_WIDTH,
+        PLAYER_TURN_SPEED,
+        PLAYER_SPEED
+)
 import pygame
 
 class Player(CircleShape):
