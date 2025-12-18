72b6799b1723e26cbf4400aec7b87fa9633b17e6
A P <67877471+beerded@users.noreply.github.com>
2025-12-18 22:12:50 +0200

    Implement collision detection

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,8 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # must override
         pass
+
+    def collides_with(self, other):
+        distance = self.position.distance_to(other.position)
+        collision_distance = self.radius + other.radius
+        return distance <= collision_distance
