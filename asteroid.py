c809ea90d1355880d05a790751391920db41f90a
A P <67877471+beerded@users.noreply.github.com>
2025-12-18 23:14:27 +0200

    Implement asteroid splitting
    Now instead of disappearing, when hit big asteroids get smaller and
    split and speed up.

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import *
+from logger import log_event
 import pygame
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,20 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        split_angle = random.uniform(20, 50)
+        first_velocity = self.velocity.rotate(split_angle) * 1.2
+        second_velocity = self.velocity.rotate(-split_angle) * 1.2
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        first_ass = Asteroid(self.position.x, self.position.y, new_radius)
+        second_ass = Asteroid(self.position.x, self.position.y, new_radius)
+
+        first_ass.velocity = first_velocity
+        second_ass.velocity = second_velocity
+
